9d0f3156cdebc91379278bb775d7eb7dd888b588
Slowcobo <spencer.coleman92@gmail.com>
2020-08-01 22:03:44 -0700

    Add TodoApp and TodoList

---
+++ src/TodoApp.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TodoList from "./TodoList";
+
+export default function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Walk dog", completed: false },
+    { id: 2, task: "Wash car", completed: true },
+    { id: 3, task: "Grow beard", completed: true },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
+}
