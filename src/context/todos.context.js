ed2a3daf611772608ed4e0c61bacbf1ad08f3e81
Slowcobo <spencer.coleman92@gmail.com>
2020-08-04 23:45:14 -0700

    Refactor local storage to reducer

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,11 +1,12 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
+import useLocalStorageReducer from "../hooks/useLocalStorageReducer";
 import todosReducer from "../reducer/todos.reducer";
 
 export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export const TodosProvider = (props) => {
-  const [todos, dispatch] = useReducer(todosReducer, []);
+  const [todos, dispatch] = useLocalStorageReducer("todos", [], todosReducer);
   return (
     <TodosContext.Provider value={todos}>
       <DispatchContext.Provider value={dispatch}>
