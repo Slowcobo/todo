98d994ecd15eba89cbabc0fbd7d90a9f145b9ebb
Slowcobo <spencer.coleman92@gmail.com>
2020-08-07 01:53:23 -0700

    Organize Navbar and Drawer components

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,39 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { TodosProvider } from "./context/todos.context";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import Navbar from "./Navbar";
+import Drawer from "./Drawer";
 import TodoDashboard from "./TodoDashboard";
-
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.default,
     display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
   },
   content: {
     flexGrow: 1,
@@ -50,37 +26,10 @@ export default function TodoApp() {
         <CssBaseline />
 
         {/* Nav Bar */}
-        <AppBar color="default" position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" noWrap>
-              Clipped drawer
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        <Navbar />
 
         {/* Nav Drawer */}
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <Toolbar />
-          <div className={classes.drawerContainer}>
-            <List>
-              {["Placeholder 1", "Placeholder 2"].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-          </div>
-        </Drawer>
+        <Drawer />
 
         {/* Main Content */}
         <main className={classes.content}>
